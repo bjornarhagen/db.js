23aef589c976bf392aa2cb6a43076b63a907387f
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-03-15 14:48:28 +0100

    Release 0.0.1

---
+++ db.js
@@ -0,0 +1,24 @@
+export const db = (connectionPool) => {
+  return {
+    select: (query, parameters) => {
+      return new Promise((resolve, reject) => {
+        try {
+          connectionPool.getConnection(function (err, connection) {
+            if (err) throw err;
+
+            connection.query(query, parameters, function (err, result) {
+              connection.release();
+              if (err) throw err;
+
+              resolve(result);
+            });
+          });
+        } catch (error) {
+          console.error("Error: " + error);
+          connection.release();
+          reject();
+        }
+      });
+    },
+  };
+};
